58424888a3a7b2f29f876888e589409f226c42f2
teklu abayneh <tekluabayneh@gmail.com>
2024-11-16 21:08:36 +0300

    Fixed typos and bugs in Searchjbo.js and ShowPosts.js

--- client/script/ShowPosts.js
+++ client/script/ShowPosts.js
@@ -1,4 +1,3 @@
-// let jobs_container = document.querySelector(".jobs_container");
 const showPostes = async () => {
   try {
     const response = await fetch("http://localhost:3000/api/allPost", {
@@ -9,8 +8,8 @@ const showPostes = async () => {
     const data = await response.json();
 
     jobs_container.innerHTML = ` 
-  ${data.result
-    .map((job) => {
+  ${data?.result
+    ?.map((job) => {
       return ` <div
               class="job_with_their_company_container bg-white rounded-3 w-75 m-auto d-flex align-items-center justify-content-between p-2 mb-3"
             > 
@@ -24,7 +23,7 @@ const showPostes = async () => {
                   <h6 class="fw-bold text-body-tertiary">${job.companie_name}</h6>
 
                   <h5 class="fw-bolder Senior_title"> 
-                  <a href="${job.companie_name}">${job.title}</a></h5>
+                  <a href="" data-job_id="${job.job_id}">${job.title}</a></h5>
 
                   <div class="d-flex text-body-tertiary">
                     <p>${job.is_remote},</p>
@@ -39,7 +38,7 @@ const showPostes = async () => {
                 id="like_second_container"
                 class="likeicon_and_minute_conainer gap-4 flex-column"
               >
-                <i class="ms-5">like icon</i>
+                <i class="material-icons ms-5 red-heart" style="color: red;  cursor: pointer;">favorite</i>
                 <p class="text-body-tertiary p-1 fw-bold">25 minute ago</p>
               </div>
             </div>`;
@@ -47,6 +46,42 @@ const showPostes = async () => {
     .join(" ")}
   
   `;
+
+    // show the job detail page and update as user clicked
+
+    let links = document.querySelectorAll(".Senior_title a");
+
+    links.forEach((jobdetail) => {
+      jobdetail.addEventListener("click", async (e) => {
+        e.preventDefault();
+
+        // get the clicked job_id and fetch with that id to show the detail
+        let getjobId = jobdetail.getAttribute("data-job_id");
+        console.log(getjobId);
+
+        try {
+          let response = await fetch("http://localhost:3000/api/getJobDetail", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              jobId: getjobId,
+            }),
+          });
+
+          let data = await response.json();
+          console.log(data);
+
+          //store the detail in localstoreage
+          localStorage.setItem("jobDetails", JSON.stringify(data.result));
+
+          window.location.href = "/Job_Board/client/pages/ShowDeatail.html";
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    });
   } catch (error) {
     console.log(error);
   }
